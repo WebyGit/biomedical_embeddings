09ce698ad353c3dbbf70d75ce013630b2c89dadb
Roman <windj007@gmail.com>
2017-06-09 06:01:01 -0400

    add multithreading

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Some stats about the preprocessed corpus:
 
     $ wc data/all_texts_clean.txt
     240718566  6588133885 48146742602 data/all_texts_clean.txt
+
+
+Before running build build_embeddings.py open it and set suitable number of workers (default is 30!)
--- build_embeddings.py
+++ build_embeddings.py
@@ -5,25 +5,28 @@ from gensim.models.word2vec import LineSentence, Word2Vec
 
 MODELS_DIR = os.path.join(os.path.dirname(__file__), 'models')
 DATA_FILE = os.path.join(os.path.dirname(__file__), 'data', 'all_texts_clean.txt')
+WORKERS = 30
 
 if not os.path.exists(MODELS_DIR):
     os.makedirs(MODELS_DIR)
 
 
 print('Building fasttext skipgram')
-fasttext.skipgram(DATA_FILE, os.path.join(MODELS_DIR, 'fasttext_skipgram'), silent=0)
+fasttext.skipgram(DATA_FILE, os.path.join(MODELS_DIR, 'fasttext_skipgram'),
+                  silent=0, thread=WORKERS)
 print('Done with fasttext skipgram')
 
 print('Building fasttext cbow')
-fasttext.cbow(DATA_FILE, os.path.join(MODELS_DIR, 'fasttext_cbow'), silent=0)
+fasttext.cbow(DATA_FILE, os.path.join(MODELS_DIR, 'fasttext_cbow'),
+              silent=0, thread=WORKERS)
 print('Done with fasttext cbow')
 
 print('Building word2vec skipgram')
-Word2Vec(LineSentence(DATA_FILE), size=300, sg=1, workers=-1) \
+Word2Vec(LineSentence(DATA_FILE), size=300, sg=1, workers=WORKERS) \
     .save(os.path.join(MODELS_DIR, 'word2vec_skipgram'))
 print('Done with word2vec skipgram')
 
 print('Building word2vec cbow')
-Word2Vec(LineSentence(DATA_FILE), size=300, sg=0, workers=-1) \
+Word2Vec(LineSentence(DATA_FILE), size=300, sg=0, workers=WORKERS) \
     .save(os.path.join(MODELS_DIR, 'word2vec_skipgram'))
 print('Done with word2vec cbow')
