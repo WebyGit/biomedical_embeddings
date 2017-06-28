09d96a152031822b0cbb0c20e5a76a6b62d88c59
Roman <windj007@gmail.com>
2017-06-28 08:56:13 -0400

    update dimension and filename

--- build_embeddings.py
+++ build_embeddings.py
@@ -5,7 +5,8 @@ from gensim.models.word2vec import LineSentence, Word2Vec
 
 MODELS_DIR = os.path.join(os.path.dirname(__file__), 'models')
 DATA_FILE = os.path.join(os.path.dirname(__file__), 'data', 'all_texts_clean.txt')
-WORKERS = 30
+WORKERS = 20
+DIMENSIONS = 300
 
 if not os.path.exists(MODELS_DIR):
     os.makedirs(MODELS_DIR)
@@ -13,12 +14,12 @@ if not os.path.exists(MODELS_DIR):
 
 print('Building fasttext skipgram')
 fasttext.skipgram(DATA_FILE, os.path.join(MODELS_DIR, 'fasttext_skipgram'),
-                  silent=0, thread=WORKERS)
+                  silent=0, thread=WORKERS, dim=DIMENSIONS)
 print('Done with fasttext skipgram')
 
 print('Building fasttext cbow')
 fasttext.cbow(DATA_FILE, os.path.join(MODELS_DIR, 'fasttext_cbow'),
-              silent=0, thread=WORKERS)
+              silent=0, thread=WORKERS, dim=DIMENSIONS)
 print('Done with fasttext cbow')
 
 print('Building word2vec skipgram')
@@ -28,5 +29,5 @@ print('Done with word2vec skipgram')
 
 print('Building word2vec cbow')
 Word2Vec(LineSentence(DATA_FILE), size=300, sg=0, workers=WORKERS) \
-    .save(os.path.join(MODELS_DIR, 'word2vec_skipgram'))
+    .save(os.path.join(MODELS_DIR, 'word2vec_cbow'))
 print('Done with word2vec cbow')
